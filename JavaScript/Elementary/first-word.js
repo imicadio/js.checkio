d5ad132a523fdaefd62a5f48bb063ad3696bc257
imicadio <mrjesio@gmail.com>
2017-12-21 12:33:04 +0100

    Update first-word.js

--- JavaScript/Elementary/first-word.js
+++ JavaScript/Elementary/first-word.js
@@ -11,8 +11,7 @@ function firstWord(a) {
             continue;
         }
         else{
-            return words[i];
-            
+            return words[i];            
         }
     }
     // your code here
