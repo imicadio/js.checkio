e70a3514e8253129a5286b574d698fe00faf5c35
imicadio <mrjesio@gmail.com>
2017-12-21 12:25:42 +0100

    Create first-word.js

---
+++ JavaScript/Elementary/first-word.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function firstWord(a) {
+    // returns the first word in a given text. 
+    var dot = a.replace(/\./g, ' ');
+    var comma = dot.replace(/,/g, '');
+    var words = comma.split(" ");
+    
+    for(var i = 0; i < words.length; i++){
+        if(words[i] === ""){
+            continue;
+        }
+        else{
+            return words[i];
+            
+        }
+    }
+    // your code here
+    
+    return 0;
+}
+
+var assert = require('assert');
+if (!global.is_checking) {
+    console.log('Example:')
+    console.log(firstWord("Hello world"))
+
+    // These "asserts" using for self-checking and not for auto-testing
+    assert.equal(firstWord("Hello.World"), "Hello")
+    assert.equal(firstWord(" a word "), "a")
+    assert.equal(firstWord("don't touch it"), "don't")
+    assert.equal(firstWord("greetings, friends"), "greetings")
+    assert.equal(firstWord("... and so on ..."), "and")
+    assert.equal(firstWord("hi"), "hi")
+    console.log("Coding complete? Click 'Check' to earn cool rewards!");
+}
